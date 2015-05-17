d6c0d9b911be17981b78188e5637a90b0edbfe75
Sergio Nunes <sbrnunes@gmail.com>
2015-05-17 01:32:50 +0100

    New exercise added: RustAndMurderer and Swap Nodes

---
+++ src/main/java/datastructures/SwapNodes.java
@@ -0,0 +1,122 @@
+package datastructures;
+
+import java.io.IOException;
+import java.util.*;
+
+public class SwapNodes
+{
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numNodes = scanner.nextInt();
+
+        TreeNode[] tree = new TreeNode[numNodes];
+
+        for(int i = 0; i < numNodes; i++)
+        {
+            int left = scanner.nextInt();
+            int right = scanner.nextInt();
+
+            if(tree[i] == null)
+            {
+                tree[i] = new TreeNode(i+1);
+            }
+
+            if(left != -1)
+            {
+                tree[left-1] = new TreeNode(left);
+                tree[i].left = tree[left-1];
+            }
+
+            if(right != -1)
+            {
+                tree[right-1] = new TreeNode(right);
+                tree[i].right = tree[right-1];
+            }
+        }
+
+        int numLevelsToSwap = scanner.nextInt();
+
+        int[] levelsToSwap = new int[numLevelsToSwap];
+
+        for(int i = 0; i < numLevelsToSwap; i++)
+        {
+            levelsToSwap[i] = scanner.nextInt();
+        }
+
+        swapTree(tree[0], levelsToSwap);
+
+    }
+
+    private static void swapTree(TreeNode node, int[] levelsToSwap)
+    {
+        Stack<TreeNode> stack = new Stack<>();
+
+        for (int levelToSwap : levelsToSwap)
+        {
+            stack.clear();
+            stack.push(node);
+            findAndSwap(stack, 0, levelToSwap);
+            System.out.println();
+        }
+    }
+
+    private static void findAndSwap(Stack<TreeNode> stack, int currentLevel, int levelToSwap)
+    {
+        currentLevel++;
+
+        TreeNode node = stack.pop();
+
+        if(node.left == null && node.right == null)
+        {
+            printNode(node);
+            return;
+        }
+
+        if(currentLevel % levelToSwap == 0)
+        {
+            swapNode(node);
+        }
+
+        if(node.left != null)
+        {
+            stack.push(node.left);
+            findAndSwap(stack, currentLevel, levelToSwap);
+        }
+
+        printNode(node);
+
+        if(node.right != null)
+        {
+            stack.push(node.right);
+            findAndSwap(stack, currentLevel, levelToSwap);
+        }
+    }
+
+    private static void swapNode(TreeNode node)
+    {
+        TreeNode tmp = node.left;
+        node.left = node.right;
+        node.right = tmp;
+    }
+
+    private static void printNode(TreeNode node)
+    {
+        System.out.print(node.id + " ");
+    }
+
+    private static class TreeNode {
+        private int id;
+        private TreeNode left;
+        private TreeNode right;
+
+
+        public TreeNode(int id)
+        {
+            this.id = id;
+        }
+
+    }
+}
---
+++ src/main/java/graphs/RustAndMurderer.java
@@ -0,0 +1,96 @@
+package graphs;
+
+import java.io.IOException;
+import java.util.*;
+
+public class RustAndMurderer
+{
+
+    public static final int VILLAGE_ROAD = 0;
+    public static final int DISCOVERED = 2;
+    public static final int ROAD_COST = 1;
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int testCases = scanner.nextInt();
+
+        for(int i = 0; i < testCases; i++) {
+            int cities = scanner.nextInt();
+            int roads = scanner.nextInt();
+
+            int[][] mainRoads = new int[cities][cities];
+
+            for (int r = 0; r < roads; r++)
+            {
+                int origin = scanner.nextInt();
+                int destination = scanner.nextInt();
+
+                mainRoads[origin - 1][destination - 1] = 1;
+            }
+
+            int start = scanner.nextInt() - 1;
+
+            int[] shortestPaths = findShortestPaths(start, mainRoads);
+
+            for (int p = 0; p < shortestPaths.length; p++)
+            {
+                if(p != start)
+                {
+                    System.out.print(shortestPaths[p] + " ");
+                }
+            }
+
+            System.out.print("\n");
+        }
+
+    }
+
+    private static int[] findShortestPaths(int start, int[][] map)
+    {
+        int[] distanceToNodes = new int[map.length];
+
+        for (int i = 0; i < distanceToNodes.length; i++)
+        {
+            distanceToNodes[i] = Integer.MAX_VALUE;
+        }
+
+        distanceToNodes[start] = 0;
+
+        Queue<Integer> queue = new PriorityQueue<>((node1, node2) -> {
+            return Integer.compare(distanceToNodes[node1], distanceToNodes[node2]);
+        });
+
+        queue.add(start);
+
+        while(!queue.isEmpty())
+        {
+            Integer node = queue.poll();
+
+            if(map[node][node] == DISCOVERED)
+            {
+                continue;
+            }
+
+            map[node][node] = DISCOVERED; //discovered
+
+            for (int neighbour = 0; neighbour < map[node].length; neighbour++)
+            {
+                if(map[neighbour][neighbour] == DISCOVERED)
+                {
+                    continue;
+                }
+
+                if(neighbour != node && map[node][neighbour] == VILLAGE_ROAD) //not main road
+                {
+                    distanceToNodes[neighbour] = Math.min(distanceToNodes[neighbour], distanceToNodes[node] + ROAD_COST);
+
+                    queue.add(neighbour);
+                }
+            }
+        }
+
+        return distanceToNodes;
+    }
+}
