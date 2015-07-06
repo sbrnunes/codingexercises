33b07570c08245548194c6bd640f131f9f591551
sbrnunes <sbrnunes@gmail.com>
2015-07-06 18:29:09 +0100

    Update HappyNumbers.java

--- src/main/java/other/HappyNumbers.java
+++ src/main/java/other/HappyNumbers.java
@@ -3,6 +3,17 @@ package other;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * A ideia deste exercicio e verificar se um determinado numero e Happy ou nao.
+ * 
+ * Um numero e happy se a soma do quadrado de cada digito desse numero e igual a 1, sendo que, em cada iteracao, esse numero 
+ * e substituido pela soma calculada. Se o numero original voltar a aparecer na soma, o numero nunca podera ser Happy.
+ * 
+ * Ex: 19 : 1^2 + 9^2 = 82
+ *     82 : 8^2 + 2^2 = 68
+ *     68 : 6^2 + 8^2 = 100
+ *     100 : 1^2 + 0 + 0 = 1 => Happy Number
+ */
 public class HappyNumbers {
 
 
